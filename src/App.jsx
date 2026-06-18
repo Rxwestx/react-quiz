552fce0ab62511f27b5834ef3245ff89385eb1f4
Rxwestx <wexriexst13@gmail.com>
2026-06-19 00:46:50 +0900

    fix: コードの整形を行い、インポート順を整理

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,19 @@ import { BrowserRouter, Route, Routes, } from 'react-router-dom'
 import './App.css'
 import { ROUTES } from './const'
 import HomePage from './pages/HomePage'
-function App() {
+import QuizPage from './pages/QuizPage'
+import ResultPage from './pages/ResultPage'
 
+function App() {
 
   return (
     <>
     <BrowserRouter>
-
-    <Routes>
-    <Route path={ROUTES.HOME} element={<HomePage />} />
-    <Route path={ROUTES.QUIZ} element={<QuizPage />} />
-    <Route path={ROUTES.RESULT} element={<ResultPage />} />
-    </Routes>
+      <Routes>
+        <Route path={ROUTES.HOME} element={<HomePage />} />
+        <Route path={ROUTES.QUIZ} element={<QuizPage />} />
+        <Route path={ROUTES.RESULT} element={<ResultPage />} />
+      </Routes>
     </BrowserRouter>
     </>
   )
