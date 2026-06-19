b22e6b34bcc218c6849d4ba13d18085c565c1ba1
Rxwestx <wexriexst13@gmail.com>
2026-06-20 00:15:11 +0900

    add: quizDataを新規作成し、クイズの質問と選択肢を追加

---
+++ src/data/quiz.js
@@ -0,0 +1,22 @@
+const quizData = [
+  {
+
+    question: "Question1",
+    options: ["select1", "select2", "select3", "select4"],
+    answerIndex: 0
+    },{
+        question: "Question2",
+options: ["select1", "select2", "select3", "select4"],
+answerIndex: 1
+    },{
+        question: "Question3",
+options: ["select1", "select2", "select3", "select4"],
+answerIndex: 2
+    },{
+        question: "Question4",
+options: ["select1", "select2", "select3", "select4"],
+answerIndex: 3
+    }
+];
+
+export default quizData
