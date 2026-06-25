58793f28eaf88af72f35ecbce4ab6f6a4593f758
Rxwestx <wexriexst13@gmail.com>
2026-06-25 19:46:56 +0900

    add: QuizPageコンポーネントにクイズの質問を表示する機能を追加

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -1,7 +1,17 @@
+import Display from "../components/Display/Display"
+import quizData from "../data/quiz"
 
 export default function QuizPage() {
+  const quizIndex = 0;
   return (
-    <div>QuizPage</div>
+    <>
+      <Display>
+          {`Q1. ${quizData[quizIndex].question}`}
+      </Display>
+    
+    </>
+
   )
 }
+
 
