2144de891be1bf06d1ed9830dbc90c51c986597b
Rxwestx <wexriexst13@gmail.com>
2026-06-25 20:10:53 +0900

    Fix:button

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -1,5 +1,6 @@
 import Display from "../components/Display/Display"
 import quizData from "../data/quiz"
+import Button from "../components/Button/Button"
 
 export default function QuizPage() {
   const quizIndex = 0;
@@ -8,7 +9,10 @@ export default function QuizPage() {
       <Display>
           {`Q1. ${quizData[quizIndex].question}`}
       </Display>
-    
+      <Button>{quizData[quizIndex].options[0]}</Button>
+      <Button>{quizData[quizIndex].options[1]}</Button>
+      <Button>{quizData[quizIndex].options[2]}</Button>
+      <Button>{quizData[quizIndex].options[3]}</Button>
     </>
 
   )
