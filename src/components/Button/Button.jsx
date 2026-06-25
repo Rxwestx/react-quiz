fcab5b9551a5e1853e838207a05b893ba09cd1c6
Rxwestx <wexriexst13@gmail.com>
2026-06-25 21:03:54 +0900

    fix: ButtonコンポーネントにonClickプロパティを追加

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,8 @@
-import styles from "./Button.module.css"
-export default function Button({ children }) {
+import styles from "./Button.module.css";
+export default function Button({ children, onClick }) {
   return (
-    <button className={styles.button} type="button">
-        <span className={styles.buttonInner}>{children}</span>
+    <button className={styles.button} type="button" onClick={onClick}>
+      <span className={styles.buttonInner}>{children}</span>
     </button>
-  )
+  );
 }
-
