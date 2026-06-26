82d8b3c8d38432b3d3ca48fba6c946345d233cda
Rxwestx <wexriexst13@gmail.com>
2026-06-26 21:24:21 +0900

    fix: コードのフォーマットを整え、可読性を向上

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -9,36 +9,41 @@ export default function QuizPage() {
   const [quizIndex, setQuizIndex] = useState(0);
   const [answersLogs, setAnswersLogs] = useState([]);
   const navigation = useNavigate();
-  const MAX_QUIZ_LEN = quizData.length
+  const MAX_QUIZ_LEN = quizData.length;
 
   const handleClick = (clickedIndex) => {
     if (clickedIndex === quizData[quizIndex].answerIndex) {
-      setAnswersLogs(prev => [...prev, true]);
+      setAnswersLogs((prev) => [...prev, true]);
     } else {
-      setAnswersLogs(prev => [...prev, false]);
+      setAnswersLogs((prev) => [...prev, false]);
     }
-    setQuizIndex(prev => prev + 1);
+    setQuizIndex((prev) => prev + 1);
   };
 
   useEffect(() => {
-    if (answersLogs.length === MAX_QUIZ_LEN){
-      const correctNum = answersLogs.filter(answer => answer === true)
-      navigation(ROUTES.RESULT,{
+    if (answersLogs.length === MAX_QUIZ_LEN) {
+      const correctNum = answersLogs.filter((answer) => answer === true);
+      navigation(ROUTES.RESULT, {
         state: {
           maxQuizLen: MAX_QUIZ_LEN,
-          correctNumLen: correctNum.length
-        }
+          correctNumLen: correctNum.length,
+        },
       });
     }
-  },[answersLogs,MAX_QUIZ_LEN,navigation]);
+  }, [answersLogs, MAX_QUIZ_LEN, navigation]);
 
   return (
     <>
-      {quizData[quizIndex] &&<Display>{
-      `Q${quizIndex +1}. ${quizData[quizIndex].question}`}</Display>}
+      {quizData[quizIndex] && (
+        <Display>{`Q${quizIndex + 1}. ${quizData[quizIndex].question}`}</Display>
+      )}
       <br />
-      {quizData[quizIndex]&& quizData[quizIndex].options.map((option, index) =>
-          <Button key={`option-${index}`} onClick={() => handleClick(index)}>{option}</Button>)}
+      {quizData[quizIndex] &&
+        quizData[quizIndex].options.map((option, index) => (
+          <Button key={`option-${index}`} onClick={() => handleClick(index)}>
+            {option}
+          </Button>
+        ))}
     </>
   );
 }
