a409473109b7ed8bfdcbb64235b80031185f7bef
Rxwestx <wexriexst13@gmail.com>
2026-06-26 18:30:00 +0900

    fix: 結果画面に正解数を表示するpropsを追加

--- src/pages/ResultPage.jsx
+++ src/pages/ResultPage.jsx
@@ -1,5 +1,6 @@
 import {Link, useLocation } from "react-router-dom"
 import { ROUTES } from "../const";
+import Result from "../components/Result/Result";
 
 export default function ResultPage() {
   const location = useLocation();
@@ -7,8 +8,7 @@ export default function ResultPage() {
   return (
     <>
       <h1>Result</h1>
-      <p>あなたの正解数は...</p>
-      <p>{`全${maxQuizLen}問中、${correctNumLen}問正解でした！`}</p>
+      <Result maxQuizLen={maxQuizLen} correctNumLen={correctNumLen} />
       <br />
       <Link to={ROUTES.QUIZ}>もう一度チャレンジ</Link>
     </>
