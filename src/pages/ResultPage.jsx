415257e8e2938d9733d85b9f5c452c53975bd9cc
Rxwestx <wexriexst13@gmail.com>
2026-06-26 18:11:45 +0900

    fix: 結果画面の表示内容を更新し、正解数を表示するように修正

--- src/pages/ResultPage.jsx
+++ src/pages/ResultPage.jsx
@@ -1,7 +1,16 @@
+import {Link, useLocation } from "react-router-dom"
+import { ROUTES } from "../const";
 
 export default function ResultPage() {
+  const location = useLocation();
+  const { maxQuizLen, correctNumLen } = location.state || {};
   return (
-    <div>ResultPage</div>
+    <>
+      <h1>Result</h1>
+      <p>あなたの正解数は...</p>
+      <p>{`全${maxQuizLen}問中、${correctNumLen}問正解でした！`}</p>
+      <br />
+      <Link to={ROUTES.QUIZ}>もう一度チャレンジ</Link>
+    </>
   )
 }
-
